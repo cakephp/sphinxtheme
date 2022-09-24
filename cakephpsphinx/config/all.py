f0f7864ac5e2a35c8e8ee93c8a74bb13e9bbf227
Mark Story <mark@mark-story.com>
2022-09-24 00:21:12 -0400

    Use built in opensearch generation
    I'll update each plugin to define this flag so that we can get
    opensearch manifests for all plugins and projects.

--- cakephpsphinx/config/all.py
+++ cakephpsphinx/config/all.py
@@ -160,7 +160,7 @@ html_sidebars = {
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-#html_use_opensearch = ''
+#html_use_opensearch = 'https://book.cakephp.org/'
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
 #html_file_suffix = None
